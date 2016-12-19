43f9ab40dcb002bed661312bfdf132c5bf7de019
Nishanth Samala <nishanth.samala@gmail.com>
2016-12-19 13:55:06 -0700

    remove unused imports on the home page

--- src/app/containers/home.page.ts
+++ src/app/containers/home.page.ts
@@ -4,12 +4,7 @@ import { Store } from '@ngrx/store'
 import { Observable } from 'rxjs/Observable'
 
 import * as fromRoot from '../reducers'
-import * as FlexFuelActions from '../actions/flexfuel.actions'
 import { FlexFuelDevice, BluetoothDevice } from '../models/devices'
-import SetupCardComponent from '../components/setup-card.component'
-import FlexFuelCardComponent from '../components/flexfuel-card.component'
-
-import { ViewPageComponent } from './view.page'
 
 @Component({
     selector: 'page-home',
